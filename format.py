e1479e9b223e80ee08d68a46fea084a4811892a4
Jason Ramo <41130059+jasonramo@users.noreply.github.com>
2018-09-17 13:02:46 -0500

    Add files via upload

---
+++ format.py
@@ -0,0 +1,14 @@
+markup = """
+<!doctype html>
+<html>
+    <head>
+        <title>{0}</title>
+    </head>
+    <body>
+        <h1>{1}</h1>
+    </body>
+</html>
+"""
+
+markup = markup.format('My Page Title', 'Page Heading')
+print(markup)
